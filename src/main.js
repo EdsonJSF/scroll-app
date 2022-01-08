1b50e0c4c56b4b76be74f03b5bc5d8047143078a
EdsonJSF <edsonjsf1990@gmail.com>
2022-01-08 07:03:29 -0400

    bootstar agregado al proyecto

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min";
+
+createApp(App).use(store).use(router).mount("#app");
