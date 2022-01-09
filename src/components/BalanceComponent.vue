69f59851646e4512b50761acb538b6a51fb23373
EdsonJSF <edsonjsf1990@gmail.com>
2022-01-09 16:17:03 -0400

    translado de la funcion balance

--- src/components/BalanceComponent.vue
+++ src/components/BalanceComponent.vue
@@ -1,32 +1,62 @@
 <template>
-  <div class="position-absolute bg-primary balance1" id="balance1"></div>
-  <div class="position-absolute bg-primary balance2" id="balance2"></div>
+  <div :key="mouseWheel"></div>
+  <div class="container d-flex justify-content-between">
+    <div class="position-relative bg-primary balance balance1"></div>
+    <div class="position-relative bg-primary balance balance2"></div>
+  </div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapState } from "vuex";
 
 export default {
   name: "BalanceComponent",
+  data() {
+    return {
+      initialPos: 35,
+      balance1: 35,
+      balance2: 35,
+    };
+  },
   methods: {
-    ...mapActions(["balance"]),
+    balance() {
+      let velocity = 5;
+      this.balance1 += this.mouseWheel * velocity;
+      this.balance2 += this.mouseWheel * -velocity;
+
+      if (this.balance1 <= 0) {
+        this.balance1 = 0;
+        this.balance2 = this.initialPos * 2;
+      }
+      if (this.balance2 <= 0) {
+        this.balance1 = this.initialPos * 2;
+        this.balance2 = 0;
+      }
+      this.changeBalance();
+    },
+    changeBalance() {
+      let balance1 = document.querySelector(".balance1");
+      let balance2 = document.querySelector(".balance2");
+      balance1.style.top = `${this.balance1}vh`;
+      balance2.style.top = `${this.balance2}vh`;
+    },
+  },
+  computed: {
+    ...mapState(["mouseWheel"]),
+  },
+  mounted() {
+    this.changeBalance();
+  },
+  updated() {
+    this.balance();
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.balance1,
-.balance2 {
+.balance {
   width: 5rem;
   height: 5rem;
   transition: all 1s;
-}
-.balance1 {
-  left: 10vw;
-  top: 45vh;
-}
-.balance2 {
-  right: 10vw;
-  top: 45vh;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,6 @@ export default createStore({
   state: {
     mouseWheel: null,
     elements: 0,
-    balance1: 44,
-    balance2: 44,
   },
   mutations: {
     updateMouseWheel(state, data) {
@@ -16,25 +14,10 @@ export default createStore({
 
     resetAll(state) {
       state.elements = 0;
-      state.balance1 = 45;
-      state.balance2 = 45;
     },
 
     putElements(state, data) {
       state.elements += data;
-    },
-
-    changeBalance(state, balance) {
-      state.balance1 += balance.data * balance.velocity;
-      state.balance2 += balance.data * -balance.velocity;
-    },
-    changeBalance1(state) {
-      state.balance1 = 0;
-      state.balance2 = 85;
-    },
-    changeBalance2(state) {
-      state.balance1 = 85;
-      state.balance2 = 0;
     },
   },
   actions: {
@@ -55,7 +38,6 @@ export default createStore({
       /* Select Function */
       let funcion = router.currentRoute._value.name;
       if (funcion === "blocks") dispatch("blocks", mouseData + 1);
-      if (funcion === "balance") dispatch("balance", mouseData);
     },
 
     blocks({ state, commit }, data) {
@@ -68,22 +50,6 @@ export default createStore({
           commit("putElements", -1);
         }
       }
-    },
-
-    balance({ state, commit }, data) {
-      let config = {
-        velocity: 5,
-        data: data,
-      };
-      commit("changeBalance", config);
-
-      if (state.balance1 <= 0) commit("changeBalance1");
-      if (state.balance2 <= 0) commit("changeBalance2");
-
-      let balance1 = document.querySelector(".balance1");
-      let balance2 = document.querySelector(".balance2");
-      balance1.style.top = `${state.balance1}vh`;
-      balance2.style.top = `${state.balance2}vh`;
     },
   },
 });
